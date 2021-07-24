19c96a365b43af14dec40a4492399fbbbffa2696
Andrey Strelin <andrejmax@yandex.ru>
2021-07-24 20:18:21 +0400

    upl task 4

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
+    include 'example_persons_array.php';
     include 'task1.php';
     include 'task2.php';
     include 'task3.php';
+    include 'task4.php';
 
     //echo getFullnameFromParts('Шматко', 'Антонина', 'Сергеевна');
     //var_dump(getPartsFromFullname('Иванов Иван Иванович'));
     //echo getShortName('Иванов Иван Иванович');
-    echo getGenderFromName('Степанова Наталья Степановна');
+    //echo getGenderFromName('Степанова Наталья Степановна');
+    var_dump(getGenderDescription($example_persons_array));
--- task4.php
+++ task4.php
@@ -2,4 +2,26 @@
 
     //Определение возрастно-полового состава
 
-    
+    function getGenderDescription($arg) {
+        $men = array_filter($arg, function($person) {
+            return getGenderFromName($person['fullname']) === 1;
+        });
+        $woman = array_filter($arg, function($person) {
+            return getGenderFromName($person['fullname']) === -1;
+        });
+        $undefined = array_filter($arg, function($person) {
+            return getGenderFromName($person['fullname']) === 0;
+        });
+        $menPercent =  round(count($men) / count($arg) * 100, 1);
+        $womanPercent =  round(count($woman) / count($arg) * 100, 1);
+        $undefinedPercent =  round(count($undefined) / count($arg) * 100, 1);
+        
+        return <<<RESULT
+        \n
+        Гендерный состав аудитории:
+        ---------------------------
+        Мужчины - $menPercent%
+        Женщины - $womanPercent%
+        Не удалось определить - $undefinedPercent%        
+        RESULT;
+    }
